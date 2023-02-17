c9e4bf44b8e65d2fd09080e2b04f09e1d277202e
playrey2010 <reynaldo.emanuel@gmail.com>
2023-02-17 12:27:49 -0800

    first commit

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+        // Create the object
+        // NOTICE SYNTAX
+
+        // INTERFACE IDENTIFIER = NEW CLASSCONSTRUCTOR()
+        // Coach theCoach = new BaseballCoach(); CHANGED to...
+        Coach theCoach = new TrackCoach(); // We used IntelliJ to autocreate TrackCoach with interface implementation
+        // Use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
